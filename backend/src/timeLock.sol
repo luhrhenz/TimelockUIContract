b7740522554b0ea344058fa1bd0d18d2260a66aa
luhrhenz <anoitinglawrence@gmail.com>
2026-03-16 10:58:26 +0100

    Deploy new contract with rewards - 0xa5EE1C526adA4D708762307Fbdec806c0B344a0C

--- backend/src/timeLock.sol
+++ backend/src/timeLock.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.33;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+interface IERC20 {
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
 
 contract TimelockSavings {
 
